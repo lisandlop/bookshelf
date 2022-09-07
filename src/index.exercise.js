fe1cfa27cf0ebec8d8f72508691f4b4c0957f7e0
Lisa Andersson López <lisa.andersson.lopez@tele2.com>
2022-09-07 11:25:38 +0200

    ex2

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,45 +1,66 @@
+import '@reach/dialog/styles.css'
 import * as React from 'react'
 import {createRoot} from 'react-dom/client'
+import {Dialog} from '@reach/dialog'
 import {Logo} from './components/logo'
 
-import { Dialog } from "@reach/dialog";
-import "@reach/dialog/styles.css";
+function LoginForm({onSubmit, buttonText}) {
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const {username, password} = event.target.elements
+    console.log(username.value)
+
+    onSubmit({
+      username: username.value, 
+      password: password.value, 
+    })
+
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username</label>
+      <input type="text" id="username" name="username"></input>
+      <label htmlFor="password">Password</label>
+      <input type="text" id="password" name="password"></input>
+      <input type="submit" value={buttonText} />
+    </form>
+  )
+}
 
 function App() {
-  const [showDialog, setShowDialog] = React.useState(false);
-  const [openModal, setOpenModal] = React.useState("none");
-  const close = () => setShowDialog(false);
+  const [openModal, setOpenModal] = React.useState('none')
+
+  const login = (formData) => {
+    console.log('login', formData)
+  }
+  const register = (formData) => {
+    console.log('register', formData)
+  }
 
   return (
     <div>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
-
       <div>
-        <button onClick={() => {
-          setShowDialog(true)
-          setOpenModal("Login")
-        }}>Login</button>
+        <button onClick={() => setOpenModal('login')}>Login</button>
       </div>
-
       <div>
-        <button onClick={() => {
-          setShowDialog(true)
-          setOpenModal("Register")
-        }}>Register</button>
+        <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
-
-      <Dialog aria-label="Registration form" isOpen={showDialog} onDismiss={close}>
-        <button 
-          className="close-button" 
-          onClick={() => {
-            setShowDialog(false);
-            setOpenModal("none")
-          }}
-        >
-          Close
-        </button>
-        <p>{openModal}</p>
+      <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Login</h3>
+        <LoginForm onSubmit={login} buttonText={'Login'}></LoginForm>
+      </Dialog>
+      <Dialog aria-label="Registration form" isOpen={openModal === 'register'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Register</h3>
+        <LoginForm onSubmit={register} buttonText={'Register'}></LoginForm>
       </Dialog>
     </div>
   )
@@ -48,3 +69,4 @@ function App() {
 const root = createRoot(document.getElementById('root'))
 root.render(<App />)
 export {root}
+
